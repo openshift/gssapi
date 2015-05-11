a251ec29b5bffab705463d8bc6c5b3441d25a858
Alex Toombs <alex.m.toombs@gmail.com>
2015-05-11 12:06:46 -0700

    Move spnego code to spnego package.

--- spnego/spnego_transport.go
+++ spnego/spnego_transport.go
@@ -1,15 +1,19 @@
 // Copyright 2013-2015 Apcera Inc. All rights reserved.
 
-package gssapi
+package spnego
 
 import (
 	"encoding/base64"
 	"fmt"
 	"net/http"
 	"strings"
+
+	"github.com/apcera/gssapi"
 )
 
-func (lib *Lib) AddSPNEGONegotiate(h http.Header, name string, token *Buffer) {
+// AddSPNEGONegotiate adds a Negotiate header with the value of a serialized
+// token to an http header.
+func AddSPNEGONegotiate(h http.Header, name string, token *gssapi.Buffer) {
 	if name == "" {
 		return
 	}
@@ -22,7 +26,9 @@ func (lib *Lib) AddSPNEGONegotiate(h http.Header, name string, token *Buffer) {
 	h.Set(name, v)
 }
 
-func (lib *Lib) CheckSPNEGONegotiate(h http.Header, name string) (present bool, token *Buffer) {
+// CheckSPNEGONegotiate checks for the presence of a Negotiate header. If
+// present, we return a gssapi Token created from the header value sent to us.
+func CheckSPNEGONegotiate(lib *gssapi.Lib, h http.Header, name string) (present bool, token *gssapi.Buffer) {
 	var err error
 	defer func() {
 		if err != nil {
@@ -36,11 +42,11 @@ func (lib *Lib) CheckSPNEGONegotiate(h http.Header, name string) (present bool, 
 	}
 
 	present = true
-	tbytes, err := base64.StdEncoding.DecodeString(
-		strings.TrimSpace(v[len("Negotiate"):]))
+	tbytes, err := base64.StdEncoding.DecodeString(strings.TrimSpace(v[len("Negotiate"):]))
 	if err != nil {
 		return false, nil
 	}
+
 	if len(tbytes) > 0 {
 		token, err = lib.MakeBufferBytes(tbytes)
 		if err != nil {
